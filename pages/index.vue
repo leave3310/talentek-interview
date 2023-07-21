8e9146fa23053149b9f320280667ebe139b70864
leave3310 <sampisam3@gmail.com>
2023-07-21 11:15:49 +0800

    [feature] complete Index Page

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="Index">
+    <KeyVision />
+    <div class="result"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+<style lang="scss">
+.Index {
+  overflow-x: hidden; 
+  background-image: url('~/assets/background.jpg');
+  background-size: cover;
+  background-position: center;
+  .result {
+    padding-top: 100px;
+  }
+}
+</style>
