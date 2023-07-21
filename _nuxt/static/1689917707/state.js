b65ef78966003acc6b978116ffb7f61b90de73cc
leave3310 <sampisam3@gmail.com>
2023-07-21 13:36:02 +0800

    557d1a3

---
+++ _nuxt/static/1689917707/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftalentek-interview\u002F_nuxt\u002Fstatic\u002F1689917707",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftalentek-interview\u002F",assetsPath:"\u002Ftalentek-interview\u002F_nuxt\u002F",cdnURL:a}}}}(null));
